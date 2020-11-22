f7dea98be8374b6230aeecdd9c4acfc7bd69eb49
Caleb <caleb.brazier@gmail.com>
2020-11-22 13:11:26 -0800

    Jump functionality
    Added a new 'Ground' layer which supports the player's 'Jump' action. Change player collision detection to continuous to prevent tunneling. Switched placeholder ground prefab to have a box collider to better support player collision when landing from a jump

---
+++ GameOff2020/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float jumpSpeed;
+    private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        if (isTouchingGround && Input.GetButtonDown("Jump"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
+    }
+}
