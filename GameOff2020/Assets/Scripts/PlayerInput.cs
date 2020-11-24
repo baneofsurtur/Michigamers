c0680f363c19eba43ed2cffc5afb8448db946621
Caleb <caleb.brazier@gmail.com>
2020-11-23 21:29:53 -0800

    Add an inputQueue to ease the timing of chaining jumps

--- GameOff2020/Assets/Scripts/PlayerInput.cs
+++ GameOff2020/Assets/Scripts/PlayerInput.cs
@@ -1,22 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
     public float speed;
-    public float movement = 0f;
-    public float jumpSpeed;
+    public int inputQueueSize = 7;
     public float jumpHeight;
     public float beatsPerMinute;
     public Transform groundCheckPoint;
     public float groundCheckRadius;
     public LayerMask groundLayer;
 
+    private float movement = 0f;
     private float secondsPerBeat;
     private float secondsPerHalfBeat;
     private bool isTouchingGround;
     private Rigidbody2D rigidBody;
     private float jumpSpeedPrivate;
     private float newAcceleration;
+    private Queue<bool> inputQueue;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,8 @@ public class PlayerInput : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         secondsPerBeat = 60f / beatsPerMinute;
         secondsPerHalfBeat = secondsPerBeat / 2f;
+
+        inputQueue = new Queue<bool>(inputQueueSize);
 
         // Given fixed jump height, calculate gravity scale needed to fall from height in half a beat
         newAcceleration = (2f * -jumpHeight) / Mathf.Pow(secondsPerHalfBeat, 2f);
@@ -36,27 +40,34 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool isJumpPressed = Input.GetButtonDown("Jump");
         isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
-        if (isTouchingGround) {
-            if (Input.GetAxis("Vertical") > 0)
-            {
-                rigidBody.position = new Vector2(rigidBody.position.x, jumpHeight);
-            }
-            if (Input.GetButtonDown("Jump"))
+
+        if (inputQueue.Count == inputQueueSize)
+        {
+            inputQueue.Dequeue();
+        }
+
+        inputQueue.Enqueue(isJumpPressed);
+
+        if (isTouchingGround)
+        {
+            if (inputQueue.Contains(true))
             {
                 rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeedPrivate);
+                inputQueue.Clear();
             }
         }
 
         // TODO: remove Support for moving the player left and right when the code that moves the level itself is checked in
         movement = Input.GetAxis("Horizontal");
-        if (movement != 0f)
+        if (movement == 0f)
         {
-            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
         else
         {
-            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
         }
     }
 }
