e197da7ac6bdd4b8d6a02d7d2c745e27db5a4a60
Caleb <caleb.brazier@gmail.com>
2020-11-29 14:43:47 -0800

    Placeholder Start Screen

---
+++ GameOff2020/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGame() => SceneManager.LoadSceneAsync(1);
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
